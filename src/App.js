49a084bec0489d62e5fefd227e9d44a1b851bad4
AdeyinkaEsi1 <yhamid2828@gmail.com>
2025-01-11 19:19:04 +0100

    upd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+function ButtonCount() {
+  const [count, setCount] =useState(0);
+
+  function handleClick() {
+    setCount(count + 1)
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times 
+    </button>
+  );
+}
 
 function App() {
   return (
@@ -20,6 +35,74 @@ function App() {
       </header>
     </div>
   );
+
 }
 
-export default App;
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Hello there.<br />How do you do?</p>
+    </>
+  );
+}
+
+
+const user = {
+  name: 'Kendrick Hughes',
+  imageUrl: '/yXOvdOSs.jpg',
+  imageSize: 100,
+}
+// yXOvdOSs
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className='avatar'
+        src={user.imageUrl}
+        alt={` ${user.name}`}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
+
+function MyButton2() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Click me
+    </button>
+  );
+}
+
+
+export default function MyApp() {
+  return (
+    <div>
+      <App />
+      <h1>Welcome to my app</h1>
+      <ButtonCount />
+      <MyButton />
+      <AboutPage />
+      <Profile />
+      <MyButton2 />
+    </div>
+  );
+}
+
+// export default App;
+// export default MyApp;
