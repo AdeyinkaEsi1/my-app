0568cce77359b30b83666375ed6bb262caabf0dc
AdeyinkaEsi1 <yhamid2828@gmail.com>
2025-01-12 09:28:21 +0100

    improv

--- src/App.js
+++ src/App.js
@@ -1,114 +1,122 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+// import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Game from './game'
 
 
 export default function MyApp() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  function handleClick() {
-    setCount(count + 1);
-  }
+  // function handleClick() {
+  //   setCount(count + 1);
+  // }
 
   return (
-    <div>
-      <App />
-      <h1>Welcome to my app</h1>
-      <div>
-        <MyButton count={count} onClick={handleClick} />
-        <MyButton count={count} onClick={handleClick} />
-      </div>
-      <MyButton />
-      <AboutPage />
-      <Profile />
-      <MyButton2 />
-    </div>
+    <BrowserRouter>
+      <Routes> 
+        <Route path='/game' element={<Game />} />
+      </Routes>
+    </BrowserRouter>
+    
+    // <div>
+    //   <App />
+    //   <h1>Welcome to my app</h1>
+    //   <div>
+    //     <MyButton count={count} onClick={handleClick} />
+    //     <MyButton count={count} onClick={handleClick} />
+    //   </div>
+    //   <MyButton />
+    //   <AboutPage />
+    //   <Profile />
+    //   <MyButton2 />
+    // </div>
   );
 }
 
-function MyButton({ count, onClick }) {
+// function MyButton({ count, onClick }) {
 
-  return (
-    <button onClick={onClick}>
-      Clicked {count} times
-    </button>
-  );
-}
-
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.-
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-
-}
+//   return (
+//     <button onClick={onClick}>
+//       Clicked {count} times
+//     </button>
+//   );
+// }
 
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.-
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
 
-
-function AboutPage() {
-  return (
-    <>
-      <h1>About</h1>
-      <p>Hello there.<br />How do you do?</p>
-    </>
-  );
-}
-
-
-const user = {
-  name: 'Kendrick Hughes',
-  imageUrl: '/yXOvdOSs.jpg',
-  imageSize: 100,
-}
-// yXOvdOSs
-
-function Profile() {
-  return (
-    <>
-      <h1>{user.name}</h1>
-      <img
-        className='avatar'
-        src={user.imageUrl}
-        alt={` ${user.name}`}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize
-        }}
-      />
-    </>
-  );
-}
-
-function MyButton2() {
-  function handleClick() {
-    alert('You clicked me!');
-  }
-
-  return (
-    <button onClick={handleClick}>
-      Click me
-    </button>
-  );
-}
+// }
 
 
 
 
-// export default App;
-// export default MyApp;
+// function AboutPage() {
+//   return (
+//     <>
+//       <h1>About</h1>
+//       <p>Hello there.<br />How do you do?</p>
+//     </>
+//   );
+// }
+
+
+// const user = {
+//   name: 'Kendrick Hughes',
+//   imageUrl: '/yXOvdOSs.jpg',
+//   imageSize: 100,
+// }
+// // yXOvdOSs
+
+// function Profile() {
+//   return (
+//     <>
+//       <h1>{user.name}</h1>
+//       <img
+//         className='avatar'
+//         src={user.imageUrl}
+//         alt={` ${user.name}`}
+//         style={{
+//           width: user.imageSize,
+//           height: user.imageSize
+//         }}
+//       />
+//     </>
+//   );
+// }
+
+// function MyButton2() {
+//   function handleClick() {
+//     alert('You clicked me!');
+//   }
+
+//   return (
+//     <button onClick={handleClick}>
+//       Click me
+//     </button>
+//   );
+// }
+
+
+
+
+// // export default App;
+// // export default MyApp;
